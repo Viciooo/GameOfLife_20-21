2ae95aa859df785c99654be47af62936a9e73f51
Viciooo <72527323+Viciooo@users.noreply.github.com>
2021-12-21 23:10:56 +0100

    backend seams to be generally completed

--- src/main/java/backend/Map.java
+++ src/main/java/backend/Map.java
@@ -9,7 +9,9 @@ public class Map {
     private final int width;
     private final int height;
     private final boolean hasBorders;
-    private HashMap<Vector2d,TreeSet<Animal>> animals;
+    private final double startEnergy;
+    private final double moveEnergy;
+    private final HashMap<Vector2d, TreeSet<Animal>> animals;
     private final HashMap<Vector2d, Grass> grasses;
     private final MapDirection[] possibleMapDirections = {
             MapDirection.N,
@@ -21,16 +23,19 @@ public class Map {
             MapDirection.W,
             MapDirection.NW
     };
-    private  double jungleRatio;
+    private double jungleRatio;
     private Vector2d jungleLeftLowerCorner;
     private Vector2d jungleRightUpperCorner;
     private ArrayList<Vector2d> allPositions;
+    private ArrayList<Animal> listOfAllAnimals;
 
-    public Map(int animalsAmount, int width, int height, double jungleRatio, boolean hasBorders, HashMap<Vector2d, TreeSet<Animal>> animals) {
+    public Map(int animalsAmount, int width, int height, double jungleRatio, boolean hasBorders, double startEnergy, double moveEnergy, HashMap<Vector2d, TreeSet<Animal>> animals) {
         this.animalsAmount = animalsAmount;
         this.width = width;
         this.height = height;
         this.hasBorders = hasBorders;
+        this.startEnergy = startEnergy;
+        this.moveEnergy = moveEnergy;
         this.animals = animals;
         this.grasses = new HashMap<>();
         createJungleAndSavannaBoundries();
@@ -38,29 +43,29 @@ public class Map {
         addGrassToJungle();
     }
 
-    public void createAllPositions(){
-        for(int x = 0;x <= this.width;x++){
-            for(int y = 0;y <= this.height;y++){
-                allPositions.add(new Vector2d(x,y));
+    public void createAllPositions() {
+        for (int x = 0; x <= this.width; x++) {
+            for (int y = 0; y <= this.height; y++) {
+                allPositions.add(new Vector2d(x, y));
             }
         }
     }
 
-    public void addGrassToJungle(){
-        for(Vector2d position: allPositions){
-            if(isInJungle(position)) grasses.put(position,new Grass());
+    public void addGrassToJungle() {
+        for (Vector2d position : allPositions) {
+            if (isInJungle(position)) grasses.put(position, new Grass());
         }
     }
 
-    public void createJungleAndSavannaBoundries(){
-        double jungleArea = width*height / (1 + 1/jungleRatio);
+    public void createJungleAndSavannaBoundries() {
+        double jungleArea = width * height / (1 + 1 / jungleRatio);
         int jungleWidth = (int) Math.sqrt(jungleArea);
         int jungleHeight = (int) Math.sqrt(jungleArea);
-        while(jungleWidth > width){
+        while (jungleWidth > width) {
             jungleWidth--;
             jungleHeight++;
         }
-        while(jungleHeight > height){
+        while (jungleHeight > height) {
             jungleWidth++;
             jungleHeight--;
         }
@@ -71,19 +76,34 @@ public class Map {
     }
 
     public void place(Animal animal) {
-        if(animals.get(animal.getPosition()) == null){
+        if (animals.get(animal.getPosition()) == null) {
             TreeSet<Animal> newTreeSet = new TreeSet<>((o1, o2) -> {
-                if(o1.equals(o2)) return 0;
-                if(o1.getEnergy() == o2.getEnergy()) return 1;
+                if (o1.equals(o2)) return 0;
+                if (o1.getEnergy() == o2.getEnergy()) return 1;
                 else return (int) (o1.getEnergy() - o2.getEnergy());
             });
             newTreeSet.add(animal);
-            animals.put(animal.getPosition(),newTreeSet);
-        }else{
+            animals.put(animal.getPosition(), newTreeSet);
+        } else {
             animals.get(animal.getPosition()).add(animal);
         }
-        grasses.remove(animal.getPosition());
         this.animalsAmount++;
+    }
+
+    public void spawnAnimal(Animal animal) {
+        if (animals.get(animal.getPosition()) == null) {
+            TreeSet<Animal> newTreeSet = new TreeSet<>((o1, o2) -> {
+                if (o1.equals(o2)) return 0;
+                if (o1.getEnergy() == o2.getEnergy()) return 1;
+                else return (int) (o1.getEnergy() - o2.getEnergy());
+            });
+            newTreeSet.add(animal);
+            animals.put(animal.getPosition(), newTreeSet);
+        } else {
+            animals.get(animal.getPosition()).add(animal);
+        }
+        this.animalsAmount++;
+        listOfAllAnimals.add(animal);
     }
 
     public void removeAnimal() {
@@ -106,7 +126,7 @@ public class Map {
         return hasBorders;
     }
 
-    public ArrayList<Animal> findStrongestAtPosition(Vector2d position){
+    public ArrayList<Animal> findStrongestAtPosition(Vector2d position) {
         ArrayList<Animal> strongestAnimals = new ArrayList<>();
         Iterator<Animal> iterator = animals.get(position).iterator();
         int i = 0;
@@ -124,34 +144,44 @@ public class Map {
     }
 
     public void feedAnimalsAtPosition(Vector2d position) {
-            if (grasses.get(position) != null) {
-                ArrayList<Animal> animalsToFeed = findStrongestAtPosition(position);
-                double energyPart = grasses.get(position).getPlantEnergy()/animalsToFeed.size();
-                grasses.remove(position);
-                for(Animal animal: animalsToFeed){
-                    animal.setEnergy(animal.getEnergy() + energyPart);
-                }
-            }
+        ArrayList<Animal> animalsToFeed = findStrongestAtPosition(position);
+        double energyPart = grasses.get(position).getPlantEnergy() / animalsToFeed.size();
+        grasses.remove(position);
+        for (Animal animal : animalsToFeed) {
+            animal.setEnergy(animal.getEnergy() + energyPart);
+        }
     }
 
-    public int[] generateGenes(Animal mommy,Animal daddy,Random rand){
+    public void feedAnimals(){
+        for(Vector2d position: grasses.keySet()){
+            feedAnimalsAtPosition(position);
+        }
+    }
+
+    public void reproduceAnimals(){
+        for(Vector2d position: animals.keySet()){
+            reproduceAnimalsAtPosition(position);
+        }
+    }
+
+    public int[] generateGenes(Animal mommy, Animal daddy, Random rand) {
         double sumOfEnergy = mommy.getEnergy() + daddy.getEnergy();
-        double energyPerOneGene = sumOfEnergy/32;
+        double energyPerOneGene = sumOfEnergy / 32;
         int[] childGenes = new int[32];
         boolean fromLeft = rand.nextInt(100) < 50;
-        if(fromLeft){
-            for(int i = 0;i < 32;i++){
-                if((i+1)*energyPerOneGene <= mommy.getEnergy()){
+        if (fromLeft) {
+            for (int i = 0; i < 32; i++) {
+                if ((i + 1) * energyPerOneGene <= mommy.getEnergy()) {
                     childGenes[i] = mommy.getGenes()[i];
-                }else{
+                } else {
                     childGenes[i] = daddy.getGenes()[i];
                 }
             }
-        }else{
-            for(int i = 0;i < 32;i++){
-                if((i+1)*energyPerOneGene <= daddy.getEnergy()){
+        } else {
+            for (int i = 0; i < 32; i++) {
+                if ((i + 1) * energyPerOneGene <= daddy.getEnergy()) {
                     childGenes[i] = daddy.getGenes()[i];
-                }else{
+                } else {
                     childGenes[i] = mommy.getGenes()[i];
                 }
             }
@@ -165,73 +195,99 @@ public class Map {
         Iterator<Animal> iterator = animals.get(position).iterator();
         int i = 0;
         while (iterator.hasNext()) {
-            if(i % 2 == 0){
+            if (i % 2 == 0) {
                 mommy = iterator.next();
-            }else{
+            } else {
                 daddy = iterator.next();
             }
             i++;
         }
-        if(mommy != null && daddy != null && !mommy.equals(daddy)){
+        if (mommy != null && daddy != null && !mommy.equals(daddy)) {
             Random rand = new Random();
-            int[] childGenes = generateGenes(mommy,daddy,rand);
+            int[] childGenes = generateGenes(mommy, daddy, rand);
             mommy.reproduce();
             daddy.reproduce();
             MapDirection childDirection = this.possibleMapDirections[rand.nextInt(8)];
             Animal child = new Animal(
-                    mommy.getEnergy()*mommy.getReproducingCost()+daddy.getEnergy()*daddy.getReproducingCost(),
+                    mommy.getEnergy() * mommy.getReproducingCost() + daddy.getEnergy() * daddy.getReproducingCost(),
                     childDirection,
                     childGenes,
                     mommy.getMoveEnergy(),
                     mommy.getMap(),
                     mommy.getPosition()
-                    );
-            place(child);
+            );
+            spawnAnimal(child);
         }
     }
 
 
-    public boolean isInJungle(Vector2d position){
+    public boolean isInJungle(Vector2d position) {
         return position.precedes(jungleLeftLowerCorner) && position.follows(jungleRightUpperCorner);
     }
 
-    public void addPlants(){
+    public void addPlants() {
         boolean jungleGrassPlaced = false;
         boolean savannaGrassPlaced = false;
-        while(!jungleGrassPlaced && !savannaGrassPlaced){
+        while (!jungleGrassPlaced && !savannaGrassPlaced) {
             int x = ThreadLocalRandom.current().nextInt(0, width);
             int y = ThreadLocalRandom.current().nextInt(0, height);
-            Vector2d grassProposition = new Vector2d(x,y);
-            if(!grasses.containsKey(grassProposition) && !animals.containsKey(grassProposition)){
-                if(isInJungle(grassProposition) && !jungleGrassPlaced){
-                    grasses.put(grassProposition,new Grass());
+            Vector2d grassProposition = new Vector2d(x, y);
+            if (!grasses.containsKey(grassProposition) && !animals.containsKey(grassProposition)) {
+                if (isInJungle(grassProposition) && !jungleGrassPlaced) {
+                    grasses.put(grassProposition, new Grass());
                     jungleGrassPlaced = true;
                 }
-                if(!isInJungle(grassProposition) && !savannaGrassPlaced){
-                    grasses.put(grassProposition,new Grass());
+                if (!isInJungle(grassProposition) && !savannaGrassPlaced) {
+                    grasses.put(grassProposition, new Grass());
                     savannaGrassPlaced = true;
                 }
             }
         }
     }
 
-    public void clearMap(){
-        for(TreeSet<Animal> animalsAtPosition: animals.values()){
-            animalsAtPosition.removeIf(Animal::isDead);
+    public void removeDeadAnimals() {
+        for (TreeSet<Animal> animalsAtPosition : animals.values()) {
+            for (Animal animal : animalsAtPosition) {
+                if (animal.isDead()) {
+                    listOfAllAnimals.remove(animal);
+                    animalsAtPosition.remove(animal);
+                }
+            }
             removeAnimal();
         }
     }
 
-    public void changePosition(Animal animal,Vector2d oldPosition){
+    public void changePosition(Animal animal, Vector2d oldPosition) {
         TreeSet<Animal> oldTreeSet = animals.get(oldPosition);
-//        TODO can couse errors because we place it and then remove from the treeSet (?)
         place(animal);
-        for(Animal a : oldTreeSet){
-            if(a == animal) {
+        for (Animal a : oldTreeSet) {
+            if (a.equals(animal)) {
                 oldTreeSet.remove(a);
                 break;
             }
         }
     }
 
+    public void moveAnimals() {
+        Random rand = new Random();
+        for (Animal animal : listOfAllAnimals) {
+            animal.move(rand.nextInt(8));
+        }
+    }
+
+    public void spawnAllAnimals() {
+        Random rand = new Random();
+        for (int i = 0; i < animalsAmount; i++) {
+            Vector2d position = new Vector2d(rand.nextInt(width + 1), rand.nextInt(height + 1));
+            int[] genes = new int[32];
+            for (int j = 0; j < 32; j++) {
+                genes[j] = rand.nextInt(8);
+            }
+            Animal newAnimal = new Animal(startEnergy, possibleMapDirections[rand.nextInt(8)], genes, moveEnergy, this, position);
+            spawnAnimal(newAnimal);
+        }
+    }
+
+
 }
+
--- src/main/java/backend/Simulation.java
+++ src/main/java/backend/Simulation.java
@@ -1,7 +1,20 @@
 package backend;
 
 public class Simulation {
-    public void run(){
+    Map map;
 
+    public Simulation(Map map) {
+        this.map = map;
+    }
+
+    public void run(){
+        map.spawnAllAnimals();
+        while (true){
+            map.removeDeadAnimals();
+            map.moveAnimals();
+            map.feedAnimals();
+            map.reproduceAnimals();
+            map.addPlants();
+        }
     }
 }
